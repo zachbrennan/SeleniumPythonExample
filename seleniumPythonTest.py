c009239c86c207d11e6527a93a4e877cfa9c76b0
Zach <zbrennan613@gmail.com>
2018-04-15 21:12:50 -0400

    testing local (not working)

--- seleniumPythonTest.py
+++ seleniumPythonTest.py
@@ -4,11 +4,12 @@ from selenium.webdriver.firefox.options import Options
 
 class SelPyTest(unittest.TestCase):
 
+    #Sets up the environment when file is executed
     def setUp(self):
         options = Options()
         
         #Comment this line out if you want to see it execute
-        options.add_argument("--headless")
+#        options.add_argument("--headless")
         
         self.driver = webdriver.Firefox(firefox_options=options)
 
@@ -38,6 +39,21 @@ class SelPyTest(unittest.TestCase):
 
         assert(driver.title == "Welcome to Python.org")
 
+    def test_local_site(self):
+        driver = self.driver
+        driver.get("file://index.html")
+    
+        print driver.title
+        
+#        plusButton = driver.find_element_by_id("controls1plus")
+#        plusButton.click()
+
+#        addTextBox = driver.find_element_by_id("itemtoadd")
+#        addTextBox.send_keys("TEST")
+
+#        print driver.find_element_by_id("item1").text
+
+    #Runs once, at end of execution 
     def tearDown(self):
         self.driver.close()
 
